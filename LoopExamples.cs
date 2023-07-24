67be5ae57cae9a2d0b243503da728ef494e175b4
Valentino <69222741+V037@users.noreply.github.com>
2023-07-24 23:04:02 +0200

    Update LoopExamples.cs

--- LoopExamples.cs
+++ LoopExamples.cs
@@ -13,33 +13,38 @@ public class LoopExamples : MonoBehaviour
 
     void Update()                    //you're calling this function for every frame your game renders
     {
+    
         //BAD LOOP - EXAMPLE N.1
         if(!useGoodLoop)                    //loop example n.1 - if usefoodloop is false then we use the bad example
         {
             for (int i = 0 ; i < 1; i++)    //"i" will get deleted every frame in Update() instance so... it will remain 0
             {                               //btw i < 5 it's the condition and the number of times you want to use for execute the loop
-                num = num + 1;
-                Debug.Log(i);               //If you need to change "i", don't do that ok? (^,^)
+                num = num + 1;                
+                Debug.Log(num);               //printing the num in the console
+                Debug.Log(i);            //If you need to change "i", don't do that ok? (^,^)
             }
         }
         else            
         {
             loopVoid();                  //call loopVoid void - if usefoodloop is false then we use the good example
         }
+
+        
         //GOOD LOOP - EXAMPLE N.2
         if(useGoodLoop)
         {
-            goodnum++;
+            goodnum++;    //basically this add 1
         }
     }
 
     void loopVoid()
     {
+    
         //GOOD LOOP - EXAMPLE N.3 
         while(goodnum2 < 100)        //nice loop
         {
             goodnum2++;            //adding 1 every time this function loopVoid() is called
-            Debug.Log(goodnum);     //printing the num in the console
+            Debug.Log(goodnum);     
         }
     }
 }
